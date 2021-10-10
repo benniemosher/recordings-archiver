df083a989067016f6fbaf8e3e05b1df011eb7f51
Bennie Mosher <benniemosher@gmail.com>
2021-10-10 13:50:12 -0600

    BAM: Change api gateway route

--- terraform/archive_raw_recordings.tf
+++ terraform/archive_raw_recordings.tf
@@ -67,7 +67,7 @@ resource "aws_apigatewayv2_integration" "archive-raw-recordings" {
 resource "aws_apigatewayv2_route" "archive-raw-recordings" {
   api_id = aws_apigatewayv2_api.lambda.id
 
-  route_key = "GET /hello"
+  route_key = "GET /archive-raw-recordings"
   target    = "integrations/${aws_apigatewayv2_integration.archive-raw-recordings.id}"
 }
 
