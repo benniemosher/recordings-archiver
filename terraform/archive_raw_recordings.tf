397734efd4a1f4681e468811a7805d314ddfb372
Bennie Mosher <benniemosher@gmail.com>
2021-10-10 13:06:01 -0600

    BAM: correct pathing

--- terraform/archive_raw_recordings.tf
+++ terraform/archive_raw_recordings.tf
@@ -1,8 +1,8 @@
 data "archive_file" "archive-raw-recordings" {
   type = "zip"
 
-  source_dir  = "${path.module}/archive-raw-recordings"
-  output_path = "${path.module}/archive-raw-recordings.zip"
+  source_dir  = "${path.module}/../archive-raw-recordings"
+  output_path = "${path.module}/../archive-raw-recordings.zip"
 }
 
 resource "aws_s3_bucket_object" "archive-raw-recordings" {
