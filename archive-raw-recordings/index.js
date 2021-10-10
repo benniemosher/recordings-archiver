d6fabf9c3a1e8b9b3d1634890844552ec01d9fcf
Bennie Mosher <benniemosher@gmail.com>
2021-10-10 14:38:14 -0600

    BAM: Revert back for now

--- archive-raw-recordings/index.js
+++ archive-raw-recordings/index.js
@@ -2,28 +2,8 @@ module.exports.handler = async (event) => {
   console.log('Event: ', event);
   let responseMessage = 'Hello, World!';
 
-  // if (event.queryStringParameters && event.queryStringParameters['Name']) {
-  //   responseMessage = 'Hello, ' + event.queryStringParameters['Name'] + '!';
-  // }
-  let body = JSON.parse(event.body)
-
-  // TODO: Change to env variable
-  if (body.headers.authorization == process.env.zoomKey) {
-    let content = JSON.parse(body.body)
-    let payload = content.payload.object
-    let topic = payload.topic
-    let email = topic.substring(
-      topic.indexOf("(") + 1,
-      topic.lastIndexOf(")")
-    )
-    let download_urls = []
-
-    payload.recording_files.forEach((file) => {
-      download_urls.push(file.download_url)
-    })
-
-    console.log(email)
-    console.log(JSON.stringify(download_urls))
+  if (event.queryStringParameters && event.queryStringParameters['Name']) {
+    responseMessage = 'Hello, ' + event.queryStringParameters['Name'] + '!';
   }
 
   return {
