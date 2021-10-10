8457991d3f783390364931eb8bcd3c3f58ac4f4f
Bennie Mosher <benniemosher@gmail.com>
2021-10-10 13:11:03 -0600

    BAM: Create lambda function

--- terraform/archive_raw_recordings.tf
+++ terraform/archive_raw_recordings.tf
@@ -13,3 +13,49 @@ resource "aws_s3_bucket_object" "archive-raw-recordings" {
 
   etag = filemd5(data.archive_file.archive-raw-recordings.output_path)
 }
+
+resource "aws_lambda_function" "archive-raw-recordings" {
+  function_name = "ArchiveRawRecordings"
+
+  s3_bucket = aws_s3_bucket.lambdas-bucket.id
+  s3_key    = aws_s3_bucket_object.archive-raw-recordings.key
+
+  runtime = "nodejs12.x"
+  handler = "index.handler"
+
+  source_code_hash = data.archive_file.archive-raw-recordings.output_base64sha256
+
+  role = aws_iam_arole.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "archive-raw-recordings" {
+  name = "/aws/lambda/${aws_lambda_function.archive-raw-recordings.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "archive-raw-recordings" {
+  name = "archive-raw-recordings-lambda"
+
+  assume_role_policy = data.aws_iam_policy_document.lambda-assume-role.json
+}
+
+data "aws_iam_policy_document" "archive-raw-recordings-assume-role" {
+  statement {
+    sid = "archive-raw-recordings-assume-role"
+
+    actions = [
+      "sts:AssumeRole"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "archive-raw-recordings-policy" {
+  role       = aws_iam_role.archive-raw-recordings.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
