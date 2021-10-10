dd1733efb36ac6b5133b68b9125b893b8d0b40c3
Bennie Mosher <benniemosher@gmail.com>
2021-10-10 13:47:55 -0600

    BAM: Add api gateway for lambda

--- terraform/archive_raw_recordings.tf
+++ terraform/archive_raw_recordings.tf
@@ -55,3 +55,33 @@ resource "aws_iam_role_policy_attachment" "archive-raw-recordings-policy" {
   role       = aws_iam_role.archive-raw-recordings.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_apigatewayv2_integration" "archive-raw-recordings" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  integration_uri    = aws_lambda_function.archive-raw-recordings.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "archive-raw-recordings" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.archive-raw-recordings.id}"
+}
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.archive-raw-recordings.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+}
